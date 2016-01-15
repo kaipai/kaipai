328a5b0f6a7d0721ca147a68d6f283800bf5914e
shengbo <tao.xu@chemayi.com>
2016-01-15 18:14:09 +0800

    fix

--- module/Api/src/Controller/v1/ProductController.php
+++ module/Api/src/Controller/v1/ProductController.php
@@ -1,19 +1,37 @@
 <?php
 namespace Api\Controller\v1;
 
+use Base\ConstDir\Api\ApiSuccess;
 use Base\Functions\Utility;
 use COM\Controller\Api;
 class ProductController extends Api{
 
     /**
-     * 商品列表
+     * 拍品列表
      */
     public function listAction(){
         $select = $this->memberModel->getSelect();
         $paginator = $this->memberModel->paginate($select);
         $paginator->setCurrentPageNumber(ceil($this->offset / $this->limit) + 1);
         $paginator->setItemCountPerPage($this->limit);
-        return $this->response(array('rows' => $paginator->getCurrentItems()->toArray(), 'total' => $paginator->getTotalItemCount()));
+        $dataRows = $paginator->getCurrentItems()->toArray();
+        $dataTotalCount = $paginator->getTotalItemCount();
+
+        return $this->response(ApiSuccess::COMMON_SUCCESS, ApiSuccess::COMMON_SUCCESS_MSG, array('rows' => $dataRows, 'total' => $dataTotalCount));
+    }
+
+    /**
+     * 拍品详情
+     */
+    public function detailAction(){
+        $productID = $this->postData['productID'];
+        $select = $this->productModel->getSelect();
+        $select->from(array('a' => 'Product'))
+            ->join(array('b' => 'ProductCategory'), 'a.productCategoryID = b.productCategoryID', 'categoryName');
+        $select->where(array('productID' => $productID));
+        $productDetail = $this->productModel->selectWith($select)->current();
+
+        return $this->response(ApiSuccess::COMMON_SUCCESS, ApiSuccess::COMMON_SUCCESS_MSG, $productDetail);
     }
 
 }
--- module/Api/src/Controller/v1/StoreController.php
+++ module/Api/src/Controller/v1/StoreController.php
@@ -1,12 +1,24 @@
 <?php
 namespace Api\Controller\v1;
 
+use Base\ConstDir\Api\ApiSuccess;
 use Base\ConstDir\BaseConst;
 use COM\Controller\Api;
 class StoreController extends Api{
 
     public function indexAction(){
         return $this->response;
+    }
+
+    public function listAction(){
+        $select = $this->storeModel->getSelect();
+        $paginator = $this->storeModel->paginate($select);
+        $paginator->setCurrentPageNumber(ceil($this->offset / $this->limit) + 1);
+        $paginator->setItemCountPerPage($this->limit);
+        $dataRows = $paginator->getCurrentItems()->toArray();
+        $dataTotalCount = $paginator->getTotalItemCount();
+
+        return $this->response(ApiSuccess::COMMON_SUCCESS, ApiSuccess::COMMON_SUCCESS_MSG, array('rows' => $dataRows, 'total' => $dataTotalCount));
     }
 
 }
