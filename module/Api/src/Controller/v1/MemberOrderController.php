8c47981b9e1d8554b50526ee40839f4f130c42eb
shengbo <tao.xu@chemayi.com>
2016-03-03 18:03:00 +0800

    fix

--- module/Api/src/Controller/v1/MemberOrderController.php
+++ module/Api/src/Controller/v1/MemberOrderController.php
@@ -42,4 +42,30 @@ class MemberOrderController extends Api{
         return $this->response(ApiSuccess::COMMON_SUCCESS, ApiSuccess::COMMON_SUCCESS_MSG);
     }
 
+    public function getDeliveryTypeAction(){
+        $delivetyTypes = $this->deliveryTypeModel->getList();
+
+        return $this->response(ApiSuccess::COMMON_SUCCESS, ApiSuccess::COMMON_SUCCESS_MSG, $delivetyTypes);
+    }
+
+    public function setDeliveryAction(){
+        $orderID = $this->request->getPost('orderID');
+        $receiverName = $this->request->getPost('receiverName');
+        $receiverMobile = $this->request->getPost('receiverMobile');
+        $receiverAddr = $this->request->getPost('receiverAddr');
+
+        if(empty($receiverName) || empty($orderID) || empty($receiverMobile) || empty($receiverAddr)) return $this->response(ApiError::PARAMETER_MISSING, ApiError::PARAMETER_MISSING_MSG);
+
+        $deliveryData = array(
+            'orderID' => $orderID,
+            'receiverName' => $receiverName,
+            'receiverMobile' => $receiverMobile,
+            'receiverAddr' => $receiverAddr,
+        );
+        $this->memberOrderModel->insert($deliveryData);
+
+        return $this->response(ApiSuccess::COMMON_SUCCESS, ApiSuccess::COMMON_SUCCESS_MSG);
+    }
+
+
 }
--- public/test.php
+++ public/test.php
@@ -1,4 +1,4 @@
-<form id="requestForm" action="http://114.55.36.109/api/v1/auction/log" target="output_frame" enctype="multipart/form-data" method="post" accept-charset="utf-8" class="form-horizontal ng-pristine ng-valid">
+<form id="requestForm" action="http://myzf.com/api/v1/member-order/get-delivery-type" target="output_frame" enctype="multipart/form-data" method="post" accept-charset="utf-8" class="form-horizontal ng-pristine ng-valid">
     <input type="hidden" name="productID" value="1" />
 
     <input type="submit" value="submit" />
