9ad4ed2d1c6865995f5a0ecdd9bd56ecc0fa5f38
shengbo <tao.xu@chemayi.com>
2016-03-04 14:59:11 +0800

    fix

--- public/test.php
+++ public/test.php
@@ -1,4 +1,5 @@
-<form id="requestForm" action="http://myzf.com/api/v1/login/get-pic-verify-code" target="output_frame" enctype="multipart/form-data" method="post" accept-charset="utf-8" class="form-horizontal ng-pristine ng-valid">
+<form id="requestForm" action="http://114.55.36.109/api/v1/login/get-pic-verify-code" target="output_frame" enctype="multipart/form-data" method="post" accept-charset="utf-8" class="form-horizontal ng-pristine ng-valid">
+    <input type="hidden" name="mobile" value="13819149852" />
 
     <input type="submit" value="submit" />
 </form>
