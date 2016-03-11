1e570e0b8f74cacbe684ad549c8cfd4c9bc395bd
shengbo <tao.xu@chemayi.com>
2016-03-11 16:53:17 +0800

    fix

--- module/Api/src/Controller/v1/CommentController.php
+++ module/Api/src/Controller/v1/CommentController.php
@@ -11,7 +11,7 @@ class CommentController extends Api{
         $paginator = $this->commentModel->paginate($select);
         $paginator->setCurrentPageNumber($this->pageNum);
         $paginator->setItemCountPerPage($this->limit);
-        $comments = $paginator->getCurrentItems();
+        $comments = $paginator->getCurrentItems()->getArrayCopy();
         $commentsCount = $paginator->getTotalItemCount();
 
         return $this->response(ApiSuccess::COMMON_SUCCESS, ApiSuccess::COMMON_SUCCESS_MSG, array('comments' => $comments, 'commentsCount' => $commentsCount));
@@ -23,6 +23,10 @@ class CommentController extends Api{
             'commentTitle' => $this->postData['commentTitle'],
             'commentContent' => $this->postData['commentContent'],
         );
+        if(!empty($this->memberInfo)){
+            $data['commenterName'] = $this->memberInfo['nickName'];
+            $data['memberID'] = $this->memberInfo['memberID'];
+        }
         $this->commentModel->insert($data);
 
         return $this->response(ApiSuccess::COMMON_SUCCESS, ApiSuccess::COMMON_SUCCESS_MSG);
