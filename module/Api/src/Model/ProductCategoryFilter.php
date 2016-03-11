d9cf1a1760da22ebf14a0dccb68a50733ea2cdf4
shengbo <tao.xu@chemayi.com>
2016-03-11 18:02:16 +0800

    fix

--- module/Api/src/Model/ProductCategoryFilter.php
+++ module/Api/src/Model/ProductCategoryFilter.php
@@ -4,19 +4,6 @@ namespace Api\Model;
 use COM\Model;
 class ProductCategoryFilter extends Model{
 
-    public function getList($where = array()){
-        $select = $this->getSelect();
-        $select->join(array('b' => 'ProductCategoryFilter'), 'ProductCategoryFilterOption.productCategoryFilterID = b.productCategoryFilterID')
-            ->where($where);
-        $tmp = $this->productCategoryFilterOptionModel->selectWith($select)->toArray();
-        $categoryFilters = array();
-        foreach($tmp as $v){
-            $categoryFilters[$v['productCategoryFilterID']]['info'] = $v;
-            $categoryFilters[$v['productCategoryFilterID']]['options'][] = $v;
-        }
 
-        return $categoryFilters;
-
-    }
 
 }
--- module/Api/src/Model/ProductCategoryFilterOption.php
+++ module/Api/src/Model/ProductCategoryFilterOption.php
@@ -3,6 +3,19 @@ namespace Api\Model;
 
 use COM\Model;
 class ProductCategoryFilterOption extends Model{
+    public function getList($where = array()){
+        $select = $this->getSelect();
+        $select->join(array('b' => 'ProductCategoryFilter'), 'ProductCategoryFilterOption.productCategoryFilterID = b.productCategoryFilterID')
+            ->where($where);
+        $tmp = $this->selectWith($select)->toArray();
+        $categoryFilters = array();
+        foreach($tmp as $v){
+            $categoryFilters[$v['productCategoryFilterID']]['info'] = $v;
+            $categoryFilters[$v['productCategoryFilterID']]['options'][] = $v;
+        }
 
+        return $categoryFilters;
+
+    }
 
 }
