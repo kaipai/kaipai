3bef4a6d2f1d15c2f95a09c3aac6b9ee160edf72
shengbo <tao.xu@chemayi.com>
2016-03-16 09:23:16 +0800

    fix

--- public/test.php
+++ public/test.php
@@ -1,6 +1,8 @@
-<form id="requestForm" action="http://myzf.com/api/v1/store-category/del" target="output_frame" enctype="multipart/form-data" method="post" accept-charset="utf-8" class="form-horizontal ng-pristine ng-valid">
-    <input style="width:100px" class="field" name="storeCategoryName" type="text">
-    <input style="width:100px" class="field" name="storeCategoryID" type="text">
-    <input style="width:100px" class="field" name="token" type="text">
+
+
+<form id="requestForm" action="http://myzf.com/api/v1/login/get-verify-code" target="output_frame" enctype="multipart/form-data" method="post" accept-charset="utf-8" class="form-horizontal ng-pristine ng-valid">
+    <input type="text" value="13819149852" name="mobile"/>
+    <input type="text" value="7432" name="picVerifyCode"/>
+    <img src="http://myzf.com/api/v1/login/getPicVerifyCode">
     <input type="submit" value="submit" />
 </form>
