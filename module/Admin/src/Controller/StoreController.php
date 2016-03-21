14b8336c246f49127c2be51a46fe63e24796eae8
shengbo <tao.xu@chemayi.com>
2016-03-21 12:53:55 +0800

    fix

--- module/Admin/src/Controller/StoreController.php
+++ module/Admin/src/Controller/StoreController.php
@@ -1,6 +1,8 @@
 <?php
 namespace Admin\Controller;
 
+use Base\ConstDir\Admin\AdminError;
+use Base\ConstDir\Api\ApiError;
 use COM\Controller\Admin;
 use Base\ConstDir\BaseConst;
 use Base\ConstDir\Admin\AdminSuccess;
@@ -24,6 +26,21 @@ class StoreController extends Admin{
         $data['rows'] = $this->storeModel->getList($where, "storeID desc", $offset, $limit);
 
         return $this->adminResponse($data);
+    }
+
+    public function verifyAction(){
+        $verifyStatus = $this->request->getPost('verifyStatus');
+        $storeID = $this->request->getPost('storeID');
+        if(empty($status)) return $this->response(AdminError::PARAMETER_MISSING, AdminError::PARAMETER_MISSING_MSG);
+        $set = array(
+            'verifyStatus' => $verifyStatus,
+        );
+        $where = array(
+            'storeID' => $storeID
+        );
+        $this->sm->get('Api\Model\Store')->update($set, $where);
+
+        return $this->response(AdminSuccess::COMMON_SUCCESS, AdminSuccess::COMMON_SUCCESS_MSG);
     }
 
 }
