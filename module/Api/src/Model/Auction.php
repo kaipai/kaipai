0c5a87e164924c0aeaa23d3d0712e74856886f3b
shengbo <tao.xu@chemayi.com>
2016-06-24 10:42:59 +0800

    fix

--- module/Api/src/Model/Auction.php
+++ module/Api/src/Model/Auction.php
@@ -8,7 +8,7 @@ use Zend\Db\Sql\Expression;
 class Auction extends Model{
 
     public function bidding($productID, $memberID, $nickName, $auctionPrice, $auctionPerPrice = 0){
-        $productInfo = $this->productModel->setColumns(array('productName'))->select(array('productID' => $productID))->current();
+        $productInfo = $this->productModel->setColumns(array('productName', 'delayType', 'specialID'))->select(array('productID' => $productID))->current();
         $proxyMember = $this->auctionMemberModel->getDetail(array('AuctionMember.productID' => $productID, 'AuctionMember.memberID != ?' => $memberID, 'AuctionMember.proxyPrice > ?' => $auctionPrice));
         try{
             $this->productModel->beginTransaction();
@@ -17,9 +17,18 @@ class Auction extends Model{
             if(!empty($topBid)){
                 $this->notificationModel->insert(array('type' => 3, 'memberID' => $topBid['memberID'], 'content' => '您的<<' . $productInfo['productName'] . '>>拍品出价被超越。'));
             }
-
-            $this->productModel->update(array('currPrice' =>  $auctionPrice), array('productID' => $productID));
-            $this->productModel->update(array('auctionCount' => new Expression('auctionCount + 1')), array('productID' => $productID));
+            $productUpdate = array(
+                'currPrice' =>  $auctionPrice,
+                'auctionCount' => new Expression('auctionCount + 1')
+            );
+            if($productInfo['delayType'] == 1){
+                $newEndTime = time() + 300;
+                $productUpdate['endTime'] = $newEndTime;
+                if(!empty($productInfo['specialID'])){
+                    $this->specialModel->update(array('endTime' => $newEndTime), array('specialID' => $productInfo['specialID']));
+                }
+            }
+            $this->productModel->update($productUpdate, array('productID' => $productID));
             $this->auctionLogModel->insert(array('productID' => $productID, 'memberID' => $memberID, 'nickName' => $nickName, 'auctionPrice' => $auctionPrice));
             $this->auctionMemberModel->existAuctionMember($productID, $memberID);
             $this->auctionMemberModel->update(array('myCurrPrice' => $auctionPrice/*, 'proxyPrice' => new Expression('null')*/), array('productID' => $productID, 'memberID' => $memberID));
