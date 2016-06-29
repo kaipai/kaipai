701c5a32a88df7eff0f5db3294657dcc8e4fd1f2
developer.xu <developer.xu@outlook.com>
2016-06-29 21:53:00 +0800

    fix

--- library/COM/Service/QrcodeService.php
+++ library/COM/Service/QrcodeService.php
@@ -9,7 +9,7 @@ class QrcodeService{
 
     public function png($url){
         $fileName = time() . '.png';
-        \QRcode::png($url, BaseRootPath . '/qrcode/' . $fileName, 0, 10);
+        \QRcode::png($url, BaseRootPath . '/qrcode/' . $fileName, 0, 6);
 
         return $fileName;
     }
