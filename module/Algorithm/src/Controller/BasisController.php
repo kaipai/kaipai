e4cc0ac68db4c6335369bf97740690072e289b8d
shengbo <tao.xu@chemayi.com>
2016-07-27 16:25:54 +0800

    fix

--- module/Algorithm/src/Controller/BasisController.php
+++ module/Algorithm/src/Controller/BasisController.php
@@ -6,32 +6,102 @@ use COM\Controller;
 class BasisController extends Controller{
 
     public function indexAction(){
-        fscanf(STDIN, "%d %s", $m, $arr);
+        //fscanf(STDIN, "%d", $n);
+
         //$result = $this->getSandClock(17);
         //$result = $this->primeNumberPair($n);
-        $arr = explode(',', $arr);
-        $result = $this->moveRight($m, $arr);
+        //$arr = explode(',', $arr);
+        //$result = $this->moveRight($m, $arr);
+        $result = $this->shuffle();
         echo $result;
 
         return $this->response;
     }
 
-    private function moveRight($m, $arr){
+    private function shuffle(){
+        $n = 2;
+        $s = '36,52,37,38,3,39,40,53,54,41,11,12,13,42,43,44,2,4,23,24,25,26,27,6,7,8,48,49,50,51,9,10,14,15,16,5,17,18,19,1,20,21,22,28,29,30,31,32,33,34,35,45,46,47';
+        $cards = array();
+        $color = array('S', 'H', 'C', 'D');
+        for($i = 0; $i < count($color); $i++){
+            for($j = 1; $j <= 13; $j++){
+                $cards[$i * 13 + $j] = $color[$i] . $j;
+            }
+        }
+        $cards[] = 'J1';
+        $cards[] = 'J2';
+
+        $s = explode(',', $s);
+        for($i = 1; $i <= $n; $i++){
+            $newCards = array();
+            for($j = 0; $j <= 53; $j++){
+                if(!empty($s[$j])){
+                    $newCards[$s[$j]] = $cards[$j + 1];
+                }else{
+                    $newCards[$j + 1] = $cards[$j + 1];
+                }
+            }
+            ksort($newCards);
+            $cards = $newCards;
+
+        }
+
+        return implode(',', $cards);
+    }
+
+    private function double($n){
+        $arr = str_split($n);
+        $arrCount = array();
         $count = count($arr);
-        for($i = $count - 1; $i >= $m; $i--){
-            if($i + $m > $count - 1){
-                $tmp = $arr[$i + $m - $count];
-                $arr[$i + $m - $count] = $arr[$i];
-                $arr[$i] = $tmp;
-                print_r($arr);
-            }else{
-                $tmp = $arr[$i];
-                $arr[$i] = $arr[$i + $m];
-                $arr[$i + $m] = $tmp;
-                print_r($arr);
+        $newArr = array();
+        $newArrCount = array();
+        $further = 0;
+        $resultStr = '';
+        $output = '';
+        for($i = $count - 1; $i >= 0; $i--){
+            $double = $arr[$i] * 2;
+            if($further){
+                $double += $further;
+            }
+            $result = $double % 10;
+            $newArr[] = $result;
+            $further = floor($double / 10);
+            $arrCount[$arr[$i]] += 1;
+        }
+        if($further){
+            $newArr[] = $further;
+        }
+
+        $newCount = count($newArr);
+        for($i = $newCount - 1; $i >= 0; $i--){
+            $resultStr .= $newArr[$i];
+            $newArrCount[$newArr[$i]] += 1;
+        }
+        $match = 'YES';
+        for($i = 0; $i < count($newArrCount) - 1; $i++){
+            if($newArrCount[$i] != $arrCount[$i]) {
+                $match = 'NO'; break;
             }
         }
 
+        $output .= $match . "\n" . $resultStr;
+        return $output;
+    }
+
+    private function moveRight($m, $arr){
+        $count = count($arr);
+        for($i = 0; $i < $count; $i++){
+            $newKey = $i + $m + $count;
+            if($newKey > 2 * $count - 1){
+                $arr[$newKey - $count] = $arr[$i];
+            }else{
+                $arr[$newKey] = $arr[$i];
+            }
+        }
+        for($i = 0; $i < $count; $i++){
+            unset($arr[$i]);
+        }
+        ksort($arr);
 
         return implode(',', $arr);
     }
